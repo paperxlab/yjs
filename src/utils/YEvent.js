7eeca03aa8b7feabacfb46e454acbb3909a237f3
KentoMoriwaki <kentomoriwaki@gmail.com>
2025-11-19 12:14:37 +0900

    Fix event check

--- src/utils/YEvent.js
+++ src/utils/YEvent.js
@@ -86,7 +86,7 @@ export class YEvent {
    */
   get keys () {
     if (this._keys === null) {
-      if (this.transaction.doc._transactionCleanups.length === 0) {
+      if (this.transaction.doc._transactionCleanups.length === 0 && (this.transaction.doc.rootDoc?._rootTransactionCleanups.length ?? 0) === 0) {
         throw error.create(errorComputeChanges)
       }
       const keys = new Map()
@@ -172,7 +172,7 @@ export class YEvent {
   get changes () {
     let changes = this._changes
     if (changes === null) {
-      if (this.transaction.doc._transactionCleanups.length === 0) {
+      if (this.transaction.doc._transactionCleanups.length === 0 && (this.transaction.doc.rootDoc?._rootTransactionCleanups.length ?? 0)  === 0) {
         throw error.create(errorComputeChanges)
       }
       const target = this.target
